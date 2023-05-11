3f0122c1c4df67a25db2b069afb90290d6bfd798
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-05-11 17:30:39 +0300

    Singleton Design Pattern

---
+++ SingletonDP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+//Creational design patterns grubuna ait singleton design pattern bir nesnenin application pool kapanana kadar bir kez üretilmesini ve tek bir instance’ının olmasını kontrol altında tutar.
+//Aynı zamanda bu nesne sınıf dışından da erişilebilir olur. Bir sınıfın bir anda sadece bir örneğinin olması istenildiği zamanlarda kullanılır.
+//Örneğin veritabanı uygulamalarında bir anda bir bağlantı nesnesinin olması sistem kaynaklarının verimli bir şekilde kullanılmasını sağlar.
+//Singleton deseni uygulanacak sınıfın constructor(yapıcı) metodu private olarak tanımlanır ve sınıfın içinde kendi türünden static bir sınıf tanımlanır.
+//Tanımlanan bu sınıfa erişimi sağlayacak bir metot veya property de sınıfa eklenir. Bu desenin birden fazla kullanım şekli olsa da genel anlamda bu şekilde kullanılır.
+namespace SingletonDP
+{
+    class Singleton
+    {
+
+        private static Singleton _instance;
+        private static Guid _id; //nesnenin tek olduğunun ispati
+
+        public Guid Id { get { return _id; } }
+
+        //singleton sınıfına ait sınıfın çalışma zamanında constructordan yararlanarak oluşturulmamamasını sağlar
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Singleton();
+
+            return _instance;
+        }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.GetInstance();
+            Console.WriteLine(singleton1.Id.ToString());
+
+            Singleton singleton2 = Singleton.GetInstance();
+            Console.WriteLine(singleton2.Id.ToString());
+
+            //ikisi için de aynı id gelir. çünkü tektir.
+            Console.ReadKey();
+        }
+
+    }
+}
