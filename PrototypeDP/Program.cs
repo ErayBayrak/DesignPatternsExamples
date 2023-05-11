7f1fbb7743ef1494f186c54efb15f54f3bcae2c6
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-05-11 17:12:37 +0300

    Prototype Design Pattern

---
+++ PrototypeDP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+//Yaptığımız projemizde nesnemizi birden fazla oluşturmamız gerektiğinde normalde “new” olarak oluşturmak yerine bir tane oluşturduğumuz nesnemizin klonunu oluşturmamızı sağlayan bir design patterndir.
+//Prototype Pattern kullanılmasının amacı üretilen nesnenin çok kaynak tüketmesi durumunun engellenmesini sağlamaktır.
+namespace PrototypeDP
+{
+    //bu classı implemente eden classlar içerideki metodu uygulamak zorunda olduklarından metot da AventurePrototype'ı döndürmek zorunda olduğundan
+    //ki kendi de abstract olduğundan türeyen tip kendi çalışma zamanını geriye döndürmekle yükümlüdür
+    abstract class AdventurePrototype
+    {
+        public abstract AdventurePrototype Clone();
+    }
+
+    //Concrete Prototype
+    class Product : AdventurePrototype
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal ListPrice { get; set; }
+
+        public Product(int productId, string name, decimal listPrice)
+        {
+            ProductId = productId;
+            Name = name;
+            ListPrice = listPrice;
+        }
+
+        public override AdventurePrototype Clone()
+        {
+            return MemberwiseClone() as AdventurePrototype;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product lcdTv = new Product(1000, "ABC Lcd TV 106", 1000);
+            //bunu ürettikten sonra yarın öbürgün bunun seri üretimine geçmek istediğimizi düşünelim.
+
+            Product lcdTv2 = lcdTv.Clone() as Product;
+            Console.WriteLine("lcdTv Orijinal:"+lcdTv.ProductId+" "+lcdTv.Name+" "+lcdTv.ListPrice+"\n lcdTv2 Kopyası: "+lcdTv2.ProductId+" "+lcdTv2.Name+" "+lcdTv2.ListPrice);
+            Console.ReadKey();
+        }
+
+    }
+}
