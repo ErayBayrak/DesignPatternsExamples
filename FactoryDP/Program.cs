9bda4cdd74d5def0686678f733aaa214cbaf8a36
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-05-18 13:42:16 +0300

    Factory Design Pattern

--- FactoryDP/Program.cs
+++ FactoryDP/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+//FACTORY DESIGN PATTERN
 //Factory Design Pattern, nesne yaratma işlemi için bir arayüz tasarlanmasını gerektirir ve alt sınıfların nesne üretmesine olanak sağlar.
 //Ayrıca, hangi sınıf nesnesinin oluşacağını da alt sınıflar kendileri belirler. Böylece nesne yaratma işlemini soyutlaştırır. 
 namespace FactoryDP
